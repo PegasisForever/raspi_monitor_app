5a33aad6052d28bd56a1e9f2537a342463375c97
Pegasis <admin@pegasis.site>
2020-07-18 00:21:39 -0400

    add splash screen

--- android/app/src/main/kotlin/site/pegasis/raspi_monitor_app/MainActivity.kt
+++ android/app/src/main/kotlin/site/pegasis/raspi_monitor_app/MainActivity.kt
@@ -2,5 +2,8 @@ package site.pegasis.raspi_monitor_app
 
 import io.flutter.embedding.android.FlutterActivity
 
+import android.os.Build
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 class MainActivity: FlutterActivity() {
 }

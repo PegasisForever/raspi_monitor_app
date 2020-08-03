549fdde97fb1d624a1e9d77989cc5442b758ea84
pegasis <admin@pegasis.site>
2020-08-02 21:44:07 -0400

    add resolve mdns

--- lib/tools.dart
+++ lib/tools.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:zeroconf/zeroconf.dart';
 
 void timedLog(String msg) {
   print('[${DateTime.now().toIso8601String()}] $msg');
@@ -15,4 +18,25 @@ String shrinkText(String text, int length) {
 bool isLandScape(BuildContext context) {
   final size = MediaQuery.of(context).size;
   return size.width > size.height;
+}
+
+Future<String> resolveMdns(String name) {
+  final trimmedName = name.split(".")[0].toLowerCase();
+  final completer = Completer<String>();
+  var zeroconf = Zeroconf(
+    onScanStarted: () => print("started scan"),
+    onScanStopped: () => completer.complete(null),
+    onServiceResolved: (Service service) {
+      if ((service.name.toLowerCase() == trimmedName ||
+          service.host.toLowerCase() == trimmedName) &&
+          service.addresses.isNotEmpty) {
+        completer.complete(service.addresses[0]);
+      }
+    },
+    onError: () => completer.completeError(null),
+  );
+
+  zeroconf.startScan(type: "_device-info._tcp");
+
+  return completer.future;
 }
